be8a77fbe16c636c91c9ee8b97c9de587995c2b3
iamgreat01 <60782671+iamgreat01@users.noreply.github.com>
2022-02-07 19:33:22 +0530

    Update Anonymous.scala

--- Scala/Practicals/Anonymous.scala
+++ Scala/Practicals/Anonymous.scala
@@ -1,1 +1,15 @@
+package example
 
+object Anonymous {
+
+  def main(args: Array[String])
+  {
+
+    var myfunc1 = (str1:String, str2:String) => str1 + str2
+
+    var myfunc2 = (_:String) + (_:String)
+
+    println(myfunc1("ABC", "EFG"))
+    println(myfunc2("ABC", "XYZ"))
+  }
+}
